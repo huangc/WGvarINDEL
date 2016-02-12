9170a29e6b35f9fcab468235c1971958c15d0842
Chun-Yuan Huang <huangcy@indiana.edu>
2016-02-12 16:47:47 -0500

    Update PREREQ.sh

--- PREREQ.sh
+++ PREREQ.sh
@@ -27,7 +27,7 @@ mkdir -p ${WORK_DIR}/scratch
 cd ${prereq_DIR}
 for i in ${SAMPLE}
 do
-ln -s ${denovo_DIR}/${i}/${i}-soap.contig .
+ln -s ${denovo_DIR}/${i}/${i}-SOAP/${i}-soap.contig .
 makeblastdb -in ${i}-soap.contig -dbtype nucl -out DB_${i}_contig -parse_seqids
 done
 
