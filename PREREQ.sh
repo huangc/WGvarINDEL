7c55d85b32bf5d0885b0f88ec981b5a849ab12a5
Chun-Yuan Huang <huangcy@indiana.edu>
2016-02-12 18:29:00 -0500

    Update PREREQ.sh

--- PREREQ.sh
+++ PREREQ.sh
@@ -28,7 +28,7 @@ cd ${prereq_DIR}
 for i in ${SAMPLE}
 do
 ln -s ${denovo_DIR}/${i}/${i}-SOAP/${i}-soap.contig .
-${BLAST_DIR}/makeblastdb -in ${i}-soap.contig -dbtype nucl -out DB_${i}_contig -parse_seqids
+makeblastdb -in ${i}-soap.contig -dbtype nucl -out DB_${i}_contig -parse_seqids
 done
 
 # Retrieve and index the reference genome
