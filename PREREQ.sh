ba97dc39f943a816546ad7390b812cbecb47e30f
Chun-Yuan Huang <huangc@users.noreply.github.com>
2016-02-22 17:18:38 -0500

    Update PREREQ.sh

--- PREREQ.sh
+++ PREREQ.sh
@@ -15,7 +15,7 @@ cd ${WORK_DIR}
 mkdir -p ${WORK_DIR}/prereq
 mkdir -p ${WORK_DIR}/doc
 mkdir -p ${WORK_DIR}/bin
-# mkdir -p ${WORK_DIR}/src
+mkdir -p ${WORK_DIR}/src
 mkdir -p ${WORK_DIR}/data
 mkdir -p ${WORK_DIR}/run
 mkdir -p ${WORK_DIR}/scratch
